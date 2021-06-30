32f8fddb76c10e144da394e0a95c2cd54d25371a
MauroBraga <M@r202150016774>
2021-06-30 09:54:45 -0300

    Content Separado

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import { useEffect, useState } from 'react';
 
 import { Button } from './components/Button';
-import { MovieCard } from './components/MovieCard';
 
 // import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
+import { Content, MovieProps } from './components/Content';
 
 import { api } from './services/api';
 
@@ -19,16 +18,7 @@ interface GenreResponseProps {
   title: string;
 }
 
-interface MovieProps {
-  imdbID: string;
-  Title: string;
-  Poster: string;
-  Ratings: Array<{
-    Source: string;
-    Value: string;
-  }>;
-  Runtime: string;
-}
+
 
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
@@ -77,19 +67,11 @@ export function App() {
 
       </nav>
 
-      <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
-      </div>
+      
+        
+          
+        <Content movies={movies} selectedGenre={selectedGenre}/>
+        
     </div>
   )
 }
